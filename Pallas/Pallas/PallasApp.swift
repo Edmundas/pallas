218e547ece2246204184898f200a3bcbd39746cc
Edmundas Matusevičius <edmundasm@gmail.com>
2022-08-30 17:26:53 +0300

    Initial project

---
+++ Pallas/Pallas/PallasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PallasApp.swift
+//  Pallas
+//
+//  Created by Edmundas Matusevičius on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct PallasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
